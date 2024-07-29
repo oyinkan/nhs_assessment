0d9ed36d48bba39297f75739b5d296acada77463
oyinkan <skusman@student.lautech.edu.ng>
2024-07-29 04:27:54 +0100

    handled form data validation for first name and last name

--- js/index.js
+++ js/index.js
@@ -1,1 +1,39 @@
-console.log("Connected");
+// Error handler
+let errorObject = {
+  fname: true,
+  lname: true,
+};
+
+// error message function
+const showError = (elementId, message, color) => {
+  let errorElement = document.getElementById(elementId);
+  errorElement.textContent = message;
+  errorElement.style.color = color;
+};
+
+// inputs change handler
+const handleChange = (id, value) => {
+  switch (id) {
+    case "fname":
+      if (value.trim() === "" || value.length < 3) {
+        errorObject.fname = true;
+        showError(`${id}_help`, "First name is invalid", "tomato");
+      } else {
+        errorObject.fname = false;
+        showError(`${id}_help`, "Add your first name", "#90EE90");
+      }
+      break;
+    case "lname":
+      if (value.trim() === "" || value.length < 3) {
+        errorObject.lname = true;
+        showError(`${id}_help`, "Last name is invalid", "tomato");
+      } else {
+        errorObject.lname = false;
+        showError(`${id}_help`, "Add your last name", "#90EE90");
+      }
+      break;
+
+    default:
+      break;
+  }
+};
